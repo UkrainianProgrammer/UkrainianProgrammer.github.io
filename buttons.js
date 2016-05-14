d65f6e7718b62a4d431b9c1d79fb4aa3722de2c6
Oleksandr Sofishchenko <insten490@gmail.com>
2016-05-13 23:10:01 -0500

    initial commit

---
+++ buttons.js
@@ -0,0 +1,6 @@
+$('.hover').click(function() {
+    $(this).removeClass('animated bounceInLeft');
+    $(this).addClass('animated bounceInRight');
+});
+
+<script src="jquery-1.12.0.min.js"></script>
