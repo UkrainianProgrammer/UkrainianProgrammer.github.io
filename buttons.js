9edbc0999fa5d82436616ae533d617e055ab0300
Oleksandr Sofishchenko <insten490@gmail.com>
2016-05-15 15:01:53 -0500

    remove

--- buttons.js
+++
@@ -1,6 +0,0 @@
-$('.hover').click(function() {
-    $(this).removeClass('animated bounceInLeft');
-    $(this).addClass('animated bounceInRight');
-});
-
-<script src="jquery-1.12.0.min.js"></script>
