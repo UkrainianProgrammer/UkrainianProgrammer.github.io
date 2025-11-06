fe44b0bbd564cc7cd19a28eb54ee1c51f7103761
Alex S <insten490@gmail.com>
2025-11-05 22:26:44 -0600

    updated Latest section

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building JS web applications",
+    title: "Currently working on next-gen in-flight entertainment systems.",
     description: "Latest in my development world",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
